70d4b6136498fcd3ae637e9929677f0ffa786d88
nitinrajputind <nitinrajput971625@gmail.com>
2024-03-18 02:09:48 +0530

    User_Model_Created

---
+++ backend/model/users.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username : {
+        type : String,
+        required : true,
+    },
+    email :{
+        type : String,
+        required : true,
+        unique : true
+    },
+    password:{
+        type : String,
+        required : true,
+    }
+},{timestamps : true})
+
+
+const USER = mongoose.model("USER", userSchema)
+
+module.exports = USER
