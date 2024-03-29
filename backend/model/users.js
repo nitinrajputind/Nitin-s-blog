413e983ecd1dc5f506d47c1498d246aa0cead7a8
nitinrajputind <nitinrajput971625@gmail.com>
2024-03-29 23:39:27 +0530

    add user delete route

--- backend/model/users.js
+++ backend/model/users.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema(
     username: {
       type: String,
       required: true,
+      unique: true,
     },
     email: {
       type: String,
--- backend/router/user.routes.js
+++ backend/router/user.routes.js
@@ -1,9 +1,10 @@
-const { test, updateUser } = require('../controller/user.controller');
-const verifyToken = require('../utils/verifyUser');
+const { test, updateUser, deleteUser } = require("../controller/user.controller");
+const verifyToken = require("../utils/verifyUser");
 
-const userRoutes = require('express').Router();
+const userRoutes = require("express").Router();
 
-userRoutes.get('/', test);
-userRoutes.put('/update/:userId',verifyToken,updateUser);
+userRoutes.get("/", test);
+userRoutes.put("/update/:userId", verifyToken, updateUser);
+userRoutes.delete("/delete/:userId", verifyToken, deleteUser);
 
 module.exports = userRoutes;
